40e2e18075293ca2100cf16cd1ce2bddcba9ee23
Sharon Frenkel <sharonfrenkel@Sharons-MacBook-Pro.local>
2022-02-20 18:52:48 +0200

    enable cors for all origins

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
     // Express serve static files on production environment
     app.use(express.static(path.resolve(__dirname, 'public')))
+    app.use(cors())
 } else {
     // Configuring CORS
     const corsOptions = {
@@ -28,7 +29,8 @@ if (process.env.NODE_ENV === 'production') {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080','http://127.0.0.1:3000', 'http://localhost:3000'],
         credentials: true
     }
-    app.use(cors(corsOptions))
+    // app.use(cors(corsOptions))
+    app.use(cors())
 }
 
 // const authRoutes = require('./api/auth/auth.routes')
